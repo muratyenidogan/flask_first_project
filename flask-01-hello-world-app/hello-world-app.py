d513bb0fa1af377fdb917bebfcbe923c1de6656a
Murat Yenidoğan <151568549+muratyenidogan@users.noreply.github.com>
2024-01-23 12:42:51 +0300

    First Commit

---
+++ flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return "hello word"
+
+@app.route('/second')
+def second():
+    return 'This is second page'
+
+@app.route('/third')
+def third():
+    return 'This is third page'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=30000)
+  # app.run(host= '0.0.0.0', port=8081)
